79d5cd6cb18cb782fdbf71e9fc9adcf1e79bab89
malek <malzovich@gmail.com>
2023-04-11 12:03:06 +0100

    Navbar UI

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import avatar from "../../public/images/placeholder.jpg";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height={30}
+      width={30}
+      alt="Avatar"
+      src={avatar}
+    />
+  );
+};
+
+export default Avatar;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,29 @@
+"use client";
 import React from "react";
 import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        <Container>hellooo</Container>
+        <Container>
+          <div
+            className="
+               flex
+               items-center
+               justify-between
+               gap-3
+               md:gap-0
+            "
+          >
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
       </div>
     </div>
   );
