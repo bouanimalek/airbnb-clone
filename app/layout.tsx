03cf4d2aaa3967544f13b52adf5d659e1ddd12e7
malek <malzovich@gmail.com>
2023-04-18 12:31:14 +0100

    Listing creation step 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
+import RentModal from "./components/modals/RentModal";
+
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
 
@@ -28,6 +31,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
+        <RentModal />
         <Navbar currentUser={currentUser} />
         {children}
       </body>
