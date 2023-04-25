f5a835a483af58bb12712f9ef7061ee2ea752993
malek <malzovich@gmail.com>
2023-04-25 16:17:07 +0100

    Trips screen (Loading trips with server component)

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -2,8 +2,7 @@
 
 import React, { useCallback, useMemo } from "react";
 import { useRouter } from "next/navigation";
-import { SafeListing, SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import { format } from "date-fns";
 import Image from "next/image";
@@ -12,7 +11,7 @@ import Button from "../Button";
 
 interface ListingCardProps {
   data: SafeListing;
-  reservation?: Reservation;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
