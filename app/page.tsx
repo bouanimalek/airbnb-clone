f44913eec5e2388199b94b4281ad3b207d9c88ce
malek <malzovich@gmail.com>
2023-04-26 16:03:44 +0100

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
@@ -9,9 +8,10 @@ import { SafeListing } from "./types";
 interface HomeProps {
   searchParams: IListingsParams;
 }
+
 const Home = async ({ searchParams }: HomeProps) => {
-  const currentUser = await getCurrentUser();
   const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return <EmptyState showReset />;
